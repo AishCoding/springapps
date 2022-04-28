07ff5ed23160377376fd11c46d046dd7b4ed2d73
AishCoding <airao@teksystems.com>
2022-04-28 13:04:47 +0530

    maven basics

---
+++ 01-springcore/src/main/java/com/springdemos/springcore/list/Hospital.java
@@ -0,0 +1,31 @@
+package com.springdemos.springcore.list;
+
+import java.util.List;
+
+public class Hospital {
+
+	@Override
+	public String toString() {
+		return "Hospital [name=" + name + ", departments=" + departments + "]";
+	}
+
+	String name;
+	List<String> departments;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<String> departments) {
+		this.departments = departments;
+	}
+
+}
